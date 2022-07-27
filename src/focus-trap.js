38705837235cc9b472194a36e0b8ce1ae67ae7dd
ryanfiller <ryanfiller89@gmail.com>
2022-07-27 16:47:31 -0500

    convert focus-trap

--- src/focus-trap.js
+++ src/focus-trap.js
@@ -1,46 +1,33 @@
 import React from 'react'
 import FocalPoint from './focal-point'
 
-const defaultProps = {
-  active: true,
-  className: 'focus-trap',
-  onExit: () => {}
-}
+export default function FocusTrap(props) {
+  const {
+    active = true,
+    className = 'focus-trap',
+    children,
+    element,
+    onExit = () => {}
+  } = props
 
-class FocusTrap extends React.Component {
-  constructor(props, context) {
-    super(props, context)
-    this._onKeyUp = this._onKeyUp.bind(this)
-  }
+  if (!active) return null
 
-  render() {
-    let { active, className, children, element, onExit } = this.props
-
-    if (!active) return null
-
-    return (
-      <div className={`${className}-wrapper`} onKeyUp={this._onKeyUp}>
-        <div
-          aria-hidden="true"
-          className={`${className}-backdrop`}
-          onClick={onExit}
-        />
-        <FocalPoint className={className} element={element}>
-          {children}
-        </FocalPoint>
-      </div>
-    )
-  }
-
-  // Private -------------------------------------------------- //
-
-  _onKeyUp(event) {
+  const onKeyUp = event => {
     if (event.key === 'Escape') {
-      this.props.onExit()
+      onExit()
     }
   }
+
+  return (
+    <div className={`${className}-wrapper`} onKeyUp={onKeyUp}>
+      <div
+        aria-hidden="true"
+        className={`${className}-backdrop`}
+        onClick={onExit}
+      />
+      <FocalPoint className={className} element={element}>
+        {children}
+      </FocalPoint>
+    </div>
+  )
 }
-
-FocusTrap.defaultProps = defaultProps
-
-export default FocusTrap
